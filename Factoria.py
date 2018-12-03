d998e1e1ece2b4c10735051750078e6a4b040d6f
Kenier Rotillet <rotilletkenier@gmail.com>
2018-12-03 14:21:44 -0500

    Clases para la factoria

--- Factoria.py
+++ Factoria.py
@@ -1,1 +1,28 @@
-Factoria.py
+class Formas(object):
+	"""docstring for FormasTrigonometricas"""
+	def conectar(self):
+		print("Conectando una forma:..")
+	def desconectar(self):
+		print("Desconectando una forma:..")
+
+class Circulo(Formas):
+	"""docstring for Circulo"""	
+	def conectar(self):
+		print("Conectando a Circulo")
+	def desconectar(self):
+		print("Desconectando a Circulo")
+
+class Cuadrado(Formas):
+	"""docstring for Circulo"""	
+	def conectar(self):
+		print("Conectando a Cuadrado")
+	def desconectar(self):
+		print("Desconectando a Cuadrado")
+
+class Triangulo(Formas):
+	"""docstring for Circulo"""	
+	def conectar(self):
+		print("Conectando a Triangulo")
+	def desconectar(self):
+		print("Desconectando a Triangulo")
+
